58e6b6a10ad012903d7ae0b66fef76e610376efa
rmourey26 <rmourey_jr@da-fi.com>
2021-10-27 10:02:45 -0400

    add bcc-sl-x509

---
+++ test/Test/Bcc/X509/Configuration.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Test.Bcc.X509.Configuration (tests) where
+
+import           Bcc.X509.Configuration
+import           Hedgehog (Gen, Property, Group, discover)
+import           Hedgehog.Internal.TH (TExpQ)
+import qualified Hedgehog as H
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+import           Bcc.Prelude hiding (head, tail)
+import           Prelude (String)
+
+-- TODO(KS): Yes, this is too complicated to import since it requires a lot.
+--roundTripTLSConfiguration :: Property
+--roundTripTLSConfiguration = roundTripsAesonYamlShow 100 genTlsConfig
+
+genTlsConfig :: Gen TLSConfiguration
+genTlsConfig = TLSConfiguration <$> genCertConfig <*> genServerConfig <*> genClients
+
+genCertConfig :: Gen CertConfiguration
+genCertConfig = CertConfiguration <$> genString <*> genString <*> genInt
+
+genServerConfig :: Gen ServerConfiguration
+genServerConfig = ServerConfiguration <$> genCertConfig <*> genNonEmptyString
+
+genClients :: Gen [CertConfiguration]
+genClients = Gen.list (Range.linearFrom 0 0 10) genCertConfig
+
+genString :: Gen String
+genString = Gen.string (Range.linearFrom 0 0 10) Gen.ascii
+
+genNonEmptyString :: Gen (NonEmpty String)
+genNonEmptyString = do
+  len <- Gen.integral (Range.linearFrom 0 0 20)
+  genNonEmpty genString len
+
+genNonEmpty :: Gen a -> Int -> Gen (NonEmpty a)
+genNonEmpty genA len = do
+  head <- genA
+  tail <- Gen.list (Range.linear 0 (fromIntegral len)) genA
+  pure (head :| tail)
+
+genInt :: Gen Int
+genInt = Gen.integral (Range.linearFrom 10 10 20)
+
+tests :: IO Bool
+tests = H.checkParallel $$(discover)
+
